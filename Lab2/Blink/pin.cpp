f1c2a9b57e88503fdaed005de8b11c8fb1118c3b
sorin36 <lica_sorin16@yahoo.com>
2017-10-24 15:40:39 +0300

    Update pin.cpp

--- Lab2/Blink/pin.cpp
+++ Lab2/Blink/pin.cpp
@@ -4,18 +4,22 @@ Pin::Pin(int pinNumber,int pinDirection,int pinState)
 {
     this -> pinNumber = pinNumber;
     this -> pinDirection =  pinDirection;
-    this -> pinState = pinState;
+    this -> pinState = pinState; // pull-up state
 }
 
 int Pin::get()
 {
-    return pinState;
+    //return pinState;
+    return bcm2835_gpio_lev(pinNumber);	
 }
 
 void Pin::set(int value)
 {
-    if(pinDirection == 1){
-        pinState = value;
+    if(pinDirection == 1){ // if output
+        if(value == 0)
+            bcm2835_gpio_clr(pinNumber)	// set pin to LOW
+        else    
+            bcm2835_gpio_set(pinNumber)	 // set pin to HIGH
     }else{
 
     }
